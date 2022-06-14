eaf5c1a7f72ac1c4a21aed830e9af27302afd7b0
ThumpChub <nicholas.vrzalik@hotmail.com>
2022-06-14 13:47:23 -0500

    Finished no arg constructor to initialize 2D array; edited getTotal() to conform to constructor changes.

--- Assignment3/ArrayOperations2D.java
+++ Assignment3/ArrayOperations2D.java
@@ -1,36 +1,46 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 class ArrayOperations2D
 {
+
+  private int[][] testArray;
+  private int firstDLength, secondDlength;
+  private final int MAX_LENGTH = 10;
+  private final int MAX_VALUE = 50;
 
   public ArrayOperations2D()
   {
     //2D int array needs to be internally initialized, not passed as an argument
     //when the class is instantiated. In the constructor, initialize a 2D array
     //using random integers. Random elements and size.
+    firstDLength = ThreadLocalRandom.current().nextInt(1, MAX_LENGTH + 1);
+    secondDlength = ThreadLocalRandom.current().nextInt(1, MAX_LENGTH + 1);
+    testArray = new int[firstDLength][secondDlength];
+
+    for(int i = 0; i < firstDLength; i++)
+    {
+      for(int j = 0; j < secondDlength; j++)
+      {
+        testArray[i][j] = ThreadLocalRandom.current().nextInt(MAX_VALUE + 1);
+      }
+    }
   }
 
 
 
-  public int getTotal(int[][] array)
+  public int getTotal()
   {
     //return the total of all values in the array
     int total = 0;
     try
     {
-      for(int i = 0; i < array.length; i++)
+      for(int i = 0; i < firstDLength; i++)
       {
-        for(int j = 0; j < array[i].length; j++)
+        for(int j = 0; j < secondDlength; j++)
         {
-          total += array[i][j];
+          total += testArray[i][j];
         }
       }
-    }
-    //Possible exceptions: array is null; array is 1D; array values are
-    //not ints
-    //Temporary catch until further refined
-    catch(Exception e)
-    {
-      //Set error message to "Invalid array argument." until further refined
-      throw new Exception("Invalid array argument.")
     }
     return total;
   }
