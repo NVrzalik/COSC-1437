2e6911cd4daea2251784080f83aefac92309af23
ThumpChub <nicholas.vrzalik@hotmail.com>
2022-06-13 23:15:26 -0500

    Framing no arg constructor

--- Assignment3/ArrayOperations2D.java
+++ Assignment3/ArrayOperations2D.java
@@ -1,6 +1,15 @@
 class ArrayOperations2D
 {
 
+  public ArrayOperations2D()
+  {
+    //2D int array needs to be internally initialized, not passed as an argument
+    //when the class is instantiated. In the constructor, initialize a 2D array
+    //using random integers. Random elements and size.
+  }
+
+
+
   public int getTotal(int[][] array)
   {
     //return the total of all values in the array
