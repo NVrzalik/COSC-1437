8b0835eb42dfbf69c0ad91967b91e28827ffa675
ThumpChub <nicholas.vrzalik@hotmail.com>
2022-06-14 15:09:24 -0500

    Removed redundant exception handling; added getAverage().

--- Assignment3/ArrayOperations2D.java
+++ Assignment3/ArrayOperations2D.java
@@ -3,25 +3,31 @@ class ArrayOperations2D
 
   public static int getTotal(int[][] array)
   {
-    if(array.length == 0)
-    {
-      throw new Exception("Array argument is empty.");
-    }
     //return the total of all values in the array
     int total = 0;
-    try
+
+    for(int i = 0; i < array.length; i++)
     {
-      for(int i = 0; i < array.length; i++)
+      for(int j = 0; j < array[i].length; j++)
       {
-        for(int j = 0; j < array[i].length; j++)
-        {
-          total += testArray[i][j];
-        }
+        total += testArray[i][j];
       }
     }
     return total;
   }
 
 
+  public static double getAverage(int[][] array)
+  {
+    int total = getTotal(array);
+    int count = 0;
 
+    for(int i = 0; i < array.length; i++)
+    {
+      count += array[i].length;
+    }
+
+    double average = (total / 1.0) / count;
+    return average;
+  }
 }
