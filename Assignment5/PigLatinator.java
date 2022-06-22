8375ff23772ff6a8bf8cc7442b1ebfb95a515034
ThumpChub <nicholas.vrzalik@hotmail.com>
2022-06-21 22:37:58 -0500

    Building PigLatinator.

--- Assignment5/PigLatinator.java
+++ Assignment5/PigLatinator.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 class PigLatinator
 {
 
@@ -14,14 +16,14 @@ class PigLatinator
   }
 
 
-  private static ArrayList<String> parseWords(StringBuilder valid)
+  private static ArrayList<String> parseWords(String input)
   {
     ArrayList<String> words = new ArrayList<String>();
     int start = 0;
-    for(int i = 0; i < valid.length(); i++)
+    for(int i = 0; i < input.length(); i++)
     {
-      char ch = valid.charAt(i);
-      if(Character.isLetterOrDigit(ch))
+      char ch = input.charAt(i);
+      if(Character.isLetterOrDigit(ch) || ch == '\'')
       {
         continue;
       }
@@ -29,14 +31,14 @@ class PigLatinator
       {
         if(i == start)
         {
-          words.add(ch);
+          words.add(String.valueOf(ch));
           start = i + 1;
         }
         else
         {
-          String word = valid.substring(start, i);
+          String word = input.substring(start, i);
           words.add(word);
-          words.add(ch);
+          words.add(String.valueOf(ch));
           start = i + 1;
         }
       }
@@ -45,15 +47,107 @@ class PigLatinator
   }
 
 
+  private static String convert(String word)
+  {
+    if(!Character.isLetterOrDigit(word.charAt(0)))
+    {
+      return word;
+    }
+
+    boolean hasNonLetters = false;
+    int i = 0;
+    while(!hasNonLetters && i < word.length())
+    {
+      char ch = word.charAt(i);
+      if(Character.isLetter(ch) || ch == '\'')
+      {
+        i++;
+        continue;
+      }
+      else
+      {
+        hasNonLetters = true;
+      }
+    }
+    if(hasNonLetters == true)
+    {
+      return word;
+    }
+
+    StringBuilder convert = new StringBuilder(word);
+
+    if(convert.length() == 1)
+    {
+      convert.append("ay");
+      return convert.toString();
+    }
+
+    if(Character.isUpperCase(convert.charAt(0)))
+    {
+      boolean allcaps = true;
+      i = 1;
+      while(allcaps && i < convert.length())
+      {
+        if(Character.isUpperCase(convert.charAt(i)))
+        {
+          i++;
+          continue;
+        }
+        else
+        {
+          allcaps = false;
+        }
+      }
+      if(allcaps == true)
+      {
+        char firstLetter = convert.charAt(0);
+        convert.deleteCharAt(0);
+        convert.append(firstLetter);
+        convert.append("AY");
+        return convert.toString();
+      }
+      else
+      {
+        char firstLetter = convert.charAt(0);
+        convert.deleteCharAt(0);
+        convert.append(firstLetter);
+        convert.append("ay");
+        firstLetter = convert.charAt(0);
+        firstLetter = Character.toUpperCase(firstLetter);
+        convert.setCharAt(0,firstLetter);
+        return convert.toString();
+      }
+    }
+
+    char firstLetter = convert.charAt(0);
+    convert.deleteCharAt(0);
+    convert.append(firstLetter);
+    convert.append("ay");
+    return convert.toString();
+  }
+
+
+
   public static String pigLatinate(String input) throws Exception
   {
-    if(invalidString())
+    if(invalidString(input))
     {
       throw new Exception("Input string is invalid");
     }
 
     input = input.trim();
-    StringBuilder valid = new StringBuilder(input);
-    ArrayList<String> words = parseWords(valid);
+    ArrayList<String> words = parseWords(input);
+
+    for(int i = 0; i < words.size(); i++)
+    {
+      words.set(i, convert(words.get(i)));
+    }
+
+    StringBuilder latin = new StringBuilder();
+    for(int i = 0; i < words.size(); i++)
+    {
+      latin.append(words.get(i));
+    }
+    return latin.toString();
   }
 }
