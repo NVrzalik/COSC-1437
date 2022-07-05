f78acd0a84882c9df039eaffc1c83a4b27e51f3e
ThumpChub <nicholas.vrzalik@hotmail.com>
2022-07-04 22:56:28 -0500

    Added documentation.

--- Assignment8/a8main.java
+++ Assignment8/a8main.java
@@ -1,13 +1,18 @@
 import javax.swing.*;
 import java.awt.*;
 
+/**This class drives the Long Distance Call Calculator, according to assignment
+specifications. This GUI program prompts the user to select a time period during
+which a long distance call took place, and then for the duration of the call in
+minutes. When these are provided, and the calculate button is clicked, the
+program calculates the price of the phone call. If the user ever forgets to
+provide the necessary input, or if the input provided is invalid, the user is
+notified and prompted again.
+The class provides the GUI frame for the program.*/
 public class a8main extends JFrame
 {
-  //private JTextField minutesField;
-  //private JRadioButton daytime;
-  //private JRadioButton evening;
-  //private JRadioButton offPeak;
 
+  /**Constructor method to create the frame.*/
   public a8main()
   {
     setTitle("Long Distance Call Calculator");
@@ -25,6 +30,7 @@ public class a8main extends JFrame
     setVisible(true);
   }
 
+  /**Main method.*/
   public static void main(String[] args)
   {
     new a8main();
