b7ee5b5de310015a3b1f66b2810476ef05ccf360
ThumpChub <nicholas.vrzalik@hotmail.com>
2022-07-04 22:48:57 -0500

    Added documentation and comments.

--- Assignment8/CalculatePanel.java
+++ Assignment8/CalculatePanel.java
@@ -1,12 +1,21 @@
 import javax.swing.*;
 import java.awt.event.*;
-import java.awt.*;
 
+/**This class creates a panel that holds the Long Distance Call Calculator's
+calculate button. It also provides an ActionEvent listener for the calculate
+button, retreiving the user's selected call rate and submitted call duration,
+and then calculating the price of the call.*/
 public class CalculatePanel extends JPanel
 {
+  //Variable to hold the RatePanel object storing the user's selection
   private RatePanel ratePanel;
+  //Variable to hold the MinutesPanel object storing the duration of the call
   private MinutesPanel minutesPanel;
 
+  /**Constructor method to create the panel. It also stores the passed RatePanel
+  and MinutesPanel object addresses in the class' fields.
+  @param ratePanel The RatePanel object storing the user's selection
+  @param minutesPanel The MinutesPanel object storing the duration of the call*/
   public CalculatePanel(RatePanel ratePanel, MinutesPanel minutesPanel)
   {
     this.ratePanel = ratePanel;
@@ -18,12 +27,18 @@ public class CalculatePanel extends JPanel
     add(calculateButton);
   }
 
+  /**Private inner class to provide an ActionEvent listener for the panel's
+  calculate button. It utilizes the accessor methods of the RatePanel and
+  MinutesPanel objects to retreive the user's selected rate and submitted call
+  duration. These values are used to calculate the price of the user's call. If
+  either the call rate or the call duration have not been provided by the user,
+  the user is notified and prompted again.*/
   private class CalculateButtonListener implements ActionListener
   {
-
+    /**The action performed by the ActionEvent listener.
+    @param event The event that triggered the ActionEvent listener.*/
     public void actionPerformed(ActionEvent event)
     {
-      //Get the rate
       int rate = 0;
       try
       {
@@ -34,7 +49,7 @@ public class CalculatePanel extends JPanel
         JOptionPane.showMessageDialog(null, "Please select a call category.");
         return;
       }
-      //Get the minutes
+
       int minutes = 0;
       try
       {
