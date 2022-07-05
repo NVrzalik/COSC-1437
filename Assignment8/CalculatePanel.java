857ab6264849359bf2a162d81fc98cbd6cd4bfa8
ThumpChub <nicholas.vrzalik@hotmail.com>
2022-07-04 21:46:02 -0500

    Built (untested) CalculatePanel class; removed unnecessary code from a8main.

---
+++ Assignment8/CalculatePanel.java
@@ -0,0 +1,56 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class CalculatePanel extends JPanel
+{
+  private RatePanel ratePanel;
+  private MinutesPanel minutesPanel;
+
+  public CalculatePanel(RatePanel ratePanel, MinutesPanel minutesPanel)
+  {
+    this.ratePanel = ratePanel;
+    this.minutesPanel = minutesPanel;
+
+    JButton calculateButton = new JButton("Calculate call price");
+    calculateButton.addActionListener(new CalculateButtonListener());
+
+    add(calculateButton);
+  }
+
+  public class CalculateButtonListener implements ActionListener
+  {
+
+    public void actionPerformed(ActionEvent event)
+    {
+      //Get the rate
+      int rate = 0;
+      try
+      {
+        rate = ratePanel.getRate();
+      }
+      catch(Exception e)
+      {
+        JOptionPane.showMessageDialog(null, "Please select a call category.");
+        return;
+      }
+      //Get the minutes
+      int minutes = 0;
+      try
+      {
+        minutes = minutesPanel.getMinutes();
+      }
+      catch(Exception e)
+      {
+        JOptionPane.showMessageDialog(null, "Please enter the duration of the" +
+          "call in minutes.");
+        return;
+      }
+
+      int cents = rate * minutes;
+      double price = cents / 100.0;
+      String results = String.format("The price of this call is $%,.2f.",
+        price);
+      JOptionPane.showMessageDialog(null, results);
+    }
+  }
+}
--- Assignment8/a8main.java
+++ Assignment8/a8main.java
@@ -8,97 +8,25 @@ public class a8main extends JFrame
   private RadioButton evening;
   private RadioButton offPeak;
 
+  public a8main()
+  {
+    setTitle("Long Distance Call Calculator");
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    RatePanel ratePanel = new RatePanel();
+    MinutesPanel minutesPanel = new MinutesPanel();
+    CalculatePanel calculatePanel = new CalculatePanel(ratePanel, minutesPanel);
+
+    add(ratePanel, BorderLayout.WEST);
+    add(minutesPanel, BorderLayout.EAST);
+    add(calculatePanel, BorderLayout.SOUTH);
+
+    pack();
+    setVisible(true);
+  }
+
   public static void main(String[] args)
   {
-    launch(args);
-  }
-
-  @Override
-  public void start(Stage stage)
-  {
-    Label optionsPrompt = new Label("Please select a call category.");
-
-    daytime = new RadioButton("Daytime (6:00AM - 5:59PM)");
-    evening = new RadioButton("Evening (6:00PM - 11:59PM)");
-    offPeak = new RadioButton("Off-Peak (12:00AM - 5:59AM)");
-
-    ToggleGroup rates = new ToggleGroup();
-    daytime.setToggleGroup(rates);
-    evening.setToggleGroup(rates);
-    offPeak.setToggleGroup(rates);
-
-    VBox options = new VBox(20, optionsPrompt, daytime, evening, offPeak);
-    options.setAlignment(Pos.LEFT);
-
-    Label minutesPrompt = new Label("How long was your phone call " +
-      "(in minutes)?");
-    minutesField = new TextField();
-    Button calculateButton = new Button("Calculate call price");
-    calculateButton.setOnAction(new CalculateButtonHandler());
-
-    VBox callLength = new VBox(10, minutesPrompt, minutesField,
-      calculateButton);
-    callLength.setAlignment(Pos.RIGHT);
-
-    HBox root = new HBox(30, options, callLength);
-    root.setAlignment(Pos.CENTER);
-    root.setPadding(new Insets(10));
-
-    Scene scene = new Scene(root);
-    stage.setScene(scene);
-    stage.setTitle("Long Distance Call Calculator");
-    stage.show();
-  }
-
-  class CalculateButtonHandler extends EventHandler<ActionEvent>
-  {
-    private final int DAYTIME_RATE_PER_MIN = 7;
-    private final int EVENING_RATE_PER_MIN = 12;
-    private final int OFF_PEAK_RATE_PER_MIN = 5;
-
-    @Override
-    public void handle(ActionEvent event)
-    {
-      if(daytime.isSelected())
-      {
-        this.calculate(DAYTIME_RATE_PER_MIN);
-      }
-      else if(evening.isSelected())
-      {
-        this.calculate(EVENING_RATE_PER_MIN);
-      }
-      else if(offPeak.isSelected())
-      {
-        this.calculate(OFF_PEAK_RATE_PER_MIN);
-      }
-      else
-      {
-        JOptionPane.showMessageDialog(null, "Please select a call category.");
-      }
-    }
-
-    private void calculate(int rate)
-    {
-      double callLength = 0;
-      try
-      {
-        this.callLength = Double.parseDouble(minutesField.getText());
-      }
-      catch(Exception e)
-      {
-        this.callLength = null;
-        JOptionPane.showMessageDialog(null, "Please enter a valid time " +
-          "amount for the call's duration.");
-      }
-
-      if(this.callLength != null)
-      {
-        this.callLength = Math.ceil(this.callLength);
-        int cents = this.callLength * rate;
-        double price = (cents / 100.0);
-        String result = String.format("This call costs $%,.2f.", price);
-        JOptionPane.showMessageDialog(null, result);
-      }
-    }
+    a8main();
   }
 }
