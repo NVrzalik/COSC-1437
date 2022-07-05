a3033fb3c781d3cdec2ba6ee21366496bf13bf3c
ThumpChub <nicholas.vrzalik@hotmail.com>
2022-07-04 20:17:56 -0500

    Built (untested) MinutesPanel class.

---
+++ Assignment8/MinutesPanel.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MinutesPanel extends JPanel
+{
+  private JTextField minutesField;
+
+  public MinutesPanel()
+  {
+    setLayout(new GridLayout(2, 1));
+
+    JLabel minutesPrompt = new JLabel("How long was your phone call " +
+      "(in minutes)?");
+
+    minutesField = new JTextField(10);
+
+    add(minutesPrompt);
+    add(minutesField);
+  }
+
+  public int getMinutes() throws Exception
+  {
+    String input = minutesField.getText();
+    double fractionalMinutes = 0;
+    try
+    {
+      fractionalMinutes = Double.parseDouble(input);
+    }
+    catch(Exception e)
+    {
+      throw new Exception("Invalid input", e);
+    }
+    int minutes = Math.ceil(fractionalMinutes);
+    return minutes;
+  }
