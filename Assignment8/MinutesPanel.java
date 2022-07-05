aac84e9fe3fd6a701117490fdb38c508ef67cf4c
ThumpChub <nicholas.vrzalik@hotmail.com>
2022-07-04 22:29:57 -0500

    Added documentation and comments.

--- Assignment8/MinutesPanel.java
+++ Assignment8/MinutesPanel.java
@@ -1,10 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
 
+/**This class creates a panel for the Long Distance Call Calculator to receive
+user input to determine the duration of a call. It prompts the user for the
+length of the phone call in minutes, and provides a textfield to allow the user
+to submit their input. It also contains a public method to return the value of
+the user's input.*/
 public class MinutesPanel extends JPanel
 {
+  //A textfield to receive input from the user
   private JTextField minutesField;
 
+  /**Constructor method to create the panel.*/
   public MinutesPanel()
   {
     setLayout(new GridLayout(2, 1));
@@ -18,6 +25,10 @@ public class MinutesPanel extends JPanel
     add(minutesField);
   }
 
+  /**Public method to return the user's input, rounded to the next integer. If
+  no input has been provided, or if the input is not a non-negative number, an
+  exception is thrown.
+  @return The length of the user's call, rounded to the next integer.*/
   public int getMinutes() throws Exception
   {
     String input = minutesField.getText();
